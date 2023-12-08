e7cdd2cef2346cff0672413765541a279ccb828c
grgkimari <grgkimari@gmail.com>
2023-12-08 21:45:29 +0300

    Created store, initialStore and actionCreator

---
+++ redux-learn/index.js
@@ -0,0 +1,29 @@
+const redux = require('redux')
+const CAKE_ORDERED = "CAKE_ORDERED"
+
+function orderCake(){
+    return {
+        type : CAKE_ORDERED,
+        quantity : 1
+    }
+}
+
+const initialState = {
+    numberOfCakes : 10
+
+}
+
+const reducer = (state = initialState, action) =>{
+    switch (action.type){
+        case CAKE_ORDERED:
+            return{
+                ...state,
+                numberOfCakes : state.numberOfCakes - 1
+            }
+        default:
+            return state
+    }
+}
+
+const store = redux.createStore(reducer)
+console.log(`initialState : ${JSON.stringify(store.getState())}`)
