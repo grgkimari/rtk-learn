82ec665a938b420e6f54188e7089332ee2713a6c
grgkimari <grgkimari@gmail.com>
2023-12-08 22:19:05 +0300

    Added Restock Action

--- redux-learn/index.js
+++ redux-learn/index.js
@@ -1,10 +1,20 @@
 const redux = require('redux')
+
+//Action Types
 const CAKE_ORDERED = "CAKE_ORDERED"
+const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 
 function orderCake(){
     return {
         type : CAKE_ORDERED,
         quantity : 1
+    }
+}
+
+function restockCakes(quantity){
+    return {
+        type : CAKE_RESTOCKED,
+        quantity 
     }
 }
 
@@ -20,6 +30,11 @@ const reducer = (state = initialState, action) =>{
                 ...state,
                 numberOfCakes : state.numberOfCakes - 1
             }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numberOfCakes : state.numberOfCakes + action.quantity
+            }
         default:
             return state
     }
@@ -33,5 +48,6 @@ console.log(`initialState : ${JSON.stringify(store.getState())}`)
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
+store.dispatch(restockCakes(5))
 
 unsubscribe()
