78662905250f9efe28599ee9a07debb0600cbc05
grgkimari <grgkimari@gmail.com>
2023-12-16 00:39:35 +0300

    Created CakeSlice

---
+++ rtk-learn/Features/Cake/CakeSlice.js
@@ -0,0 +1,24 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+
+const initialState = {
+    numberOfCakes : 10
+}
+
+
+export const CakeSlice = createSlice({
+name : "CakeSlice",
+initialState ,
+reducers : {
+    ordered : (state, action) => {
+        state.numberOfCakes -= action.payload
+    }
+,
+    restocked : (state, action) => {
+        state.numberOfCakes += action.payload
+    }
+}
+})
+
+module.exports = CakeSlice.reducer
+
+module.exports.cakeActions = CakeSlice.actions
